026ff076fcf81999edc9054728c9b3cf382b9491
Jack <jack.liu.1@stonybrook.edu>
2021-09-23 14:42:59 -0400

    Implemented Item Loading

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,18 +2,45 @@ import React from "react";
 
 export default class Workspace extends React.Component {
     render() {
-        return (
-            <div id="top5-workspace">
-                <div id="workspace-edit">
-                    <div id="edit-numbering">
-                        <div className="item-number">1.</div>
-                        <div className="item-number">2.</div>
-                        <div className="item-number">3.</div>
-                        <div className="item-number">4.</div>
-                        <div className="item-number">5.</div>
+        const { currentList } = this.props;
+        if (currentList) {
+            return (
+                <div id="top5-workspace">
+                    <div id="workspace-edit">
+                        <div id="edit-numbering">
+                            <div className="item-number">1.</div>
+                            <div className="item-number">2.</div>
+                            <div className="item-number">3.</div>
+                            <div className="item-number">4.</div>
+                            <div className="item-number">5.</div>
+                        </div>
+                        <div id ="edit-items"> {/* potentially do a map function like in sidebar.js to pass function */}
+                            <div className="top5-item">{currentList["items"][0]}</div>
+                            <div className="top5-item">{currentList["items"][1]}</div>
+                            <div className="top5-item">{currentList["items"][2]}</div>
+                            <div className="top5-item">{currentList["items"][3]}</div>
+                            <div className="top5-item">{currentList["items"][4]}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        } else{
+            return (
+                <div id="top5-workspace">
+                    <div id="workspace-edit">
+                        <div id="edit-numbering">
+                            <div className="item-number">1.</div>
+                            <div className="item-number">2.</div>
+                            <div className="item-number">3.</div>
+                            <div className="item-number">4.</div>
+                            <div className="item-number">5.</div>
+                        </div>
+                        <div id ="edit-items">
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+       
     }
 }
