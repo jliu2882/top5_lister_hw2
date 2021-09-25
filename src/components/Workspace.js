eb74d190d6eb303da8768dd0ac99be942780a4cd
Jack <jack.liu.1@stonybrook.edu>
2021-09-24 20:53:34 -0400

    Added transactions as well as comments on future updates

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -14,7 +14,15 @@ export default class Workspace extends React.Component {
                             <div className="item-number">4.</div>
                             <div className="item-number">5.</div>
                         </div>
-                        <div id ="edit-items"> {/* potentially do a map function like in sidebar.js to pass function */}
+                        <div id ="edit-items">
+                            {/* <input
+                                autoFocus
+                                className="top5-item"
+                                onKeyPress={this.handleKeyPress}
+                                onBlur={this.handleBlur}
+                                onChange={this.handleUpdate}
+                                defaultValue={currentList["items"][0]}
+                            /> */} 
                             <div className="top5-item">{currentList["items"][0]}</div>
                             <div className="top5-item">{currentList["items"][1]}</div>
                             <div className="top5-item">{currentList["items"][2]}</div>
